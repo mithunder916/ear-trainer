61cb8139adee24ea34ecf72c84a14486024c148b
Mithun <mithun@spotify.com>
2019-08-19 18:12:29 -0400

    enabled audio playback

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import * as Sound from 'react-native-sound';
+import { Audio } from 'expo-av';
 
 const intervals = [
   // 'U',
@@ -17,8 +19,73 @@ const intervals = [
   'P8',
 ];
 
+const keys = [
+  'A',
+  'Bb',
+  'B',
+  'C',
+  'Db',
+  'D',
+  'Eb',
+  'E',
+  'F',
+  'Gb',
+  'G',
+  'Ab',
+];
+
+const C_CADENCE = './assets/audio/C_cadence.mp3';
+const Db_CADENCE = './assets/audio/Db_cadence.mp3';
+const D_CADENCE = './assets/audio/D_cadence.mp3';
+const Eb_CADENCE = './assets/audio/Eb_cadence.mp3';
+const E_CADENCE = './assets/audio/E_cadence.mp3';
+const F_CADENCE = './assets/audio/F_cadence.mp3';
+const Gb_CADENCE = './assets/audio/Gb_cadence.mp3';
+const G_CADENCE = './assets/audio/G_cadence.mp3';
+const Ab_CADENCE = './assets/audio/Ab_cadence.mp3';
+const A_CADENCE = './assets/audio/A_cadence.mp3';
+const Bb_CADENCE = './assets/audio/Bb_cadence.mp3';
+const B_CADENCE = './assets/audio/B_cadence.mp3';
+
+const cadenceMap = {
+  'C': require('./assets/audio/C_cadence.mp3'),
+  'Db': require('./assets/audio/Db_cadence.mp3'),
+  'D': require('./assets/audio/D_cadence.mp3'),
+  'Eb': require('./assets/audio/Eb_cadence.mp3'),
+  'E': require('./assets/audio/E_cadence.mp3'),
+  'F': require('./assets/audio/F_cadence.mp3'),
+  'Gb': require('./assets/audio/Gb_cadence.mp3'),
+  'G': require('./assets/audio/G_cadence.mp3'),
+  'Ab': require('./assets/audio/Ab_cadence.mp3'),
+  'A': require('./assets/audio/A_cadence.mp3'),
+  'Bb': require('./assets/audio/Bb_cadence.mp3'),
+  'B': require('./assets/audio/B_cadence.mp3'),
+}
+
 export default function App() {
   const [score, setScore] = useState(0);
+  const [key, setKey] = useState('C');
+
+  async function playSound(key: string) {
+    // const soundObject = new Audio.Sound();
+    const audioUrl = cadenceMap[key];
+    try {
+      // await soundObject.loadAsync(audioUrl);
+      // await soundObject.playAsync();
+      const { sound: soundObject, status } = await Audio.Sound.createAsync(
+        audioUrl,
+        { shouldPlay: true }
+      );
+
+      const intervalNote = await Audio.Sound.createAsync(
+        require('./assets/audio/E.mp3'),
+        { shouldPlay: true }
+      );
+
+    } catch (error) {
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.score}>Score: {score}</Text>
@@ -29,7 +96,11 @@ export default function App() {
           </TouchableOpacity>
         ))}
       </View>
-      <TouchableOpacity onPress={() => setScore(score + 1)}>
+      <TouchableOpacity onPress={() => {
+        const newKey = keys[Math.floor(Math.random() * keys.length)];
+        setKey(newKey);
+        playSound(newKey);
+      }}>
         <Image style={styles.play} source={require('./assets/play-icon.png')} />
       </TouchableOpacity>
     </View>
@@ -64,7 +135,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   }
-  // score: {
-  //   display: 'block',
-  // }
 });
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Ideas
 - multiple modes - drone, etc.
 - use generative audio rather than static audio files (start with just the tones, not the cadences)
+  - currently using C2-B2
   - frequency of note * Math.pow(2, # of semitones/12) = new note frequency
